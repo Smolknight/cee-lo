70d2536826c8ee28a1d28f43c029fe01a3b675d4
Matthew Mendoza <kylethechickenman02@gmail.com>
2025-02-10 17:22:41 -0700

    markdown added

---
+++ cee-lo.md
@@ -0,0 +1,8 @@
+# Matthew Mendoza's game of cee-lo
+
+---
+
+## Objects
+
+### players
+This object is used to store the data between the player and the robot
--- roll.js
+++ roll.js
@@ -1,6 +1,9 @@
 let pot=document.getElementById('pot')
 pot.innerHTML=`<h1>Pot: 0</h1>`
+potValue=0
+
 let players={
+
     bot:{
         points:1000,
         turn:true,
@@ -16,12 +19,12 @@ let players={
 }
 
 let dice={
-    // d1:4,
-    // d2:5,
-    // d3:6
-    d1:'',
-    d2:'',
-    d3:''
+    d1:4,
+    d2:5,
+    d3:4
+    // d1:'',
+    // d2:'',
+    // d3:''
 }
 
 console.log(dice.d1, dice.d2, dice.d3)
@@ -30,14 +33,15 @@ document.getElementById("dice").innerHTML=`${dice.d1} ${dice.d2} ${dice.d3}`
 //this function checks the results of the dice roll
 function checkVal(){
 
-    dice.d1=Math.floor(1+Math.random()*6)
-    dice.d2=Math.floor(1+Math.random()*6)
-    dice.d3=Math.floor(1+Math.random()*6)
+    // dice.d1=Math.floor(1+Math.random()*6)
+    // dice.d2=Math.floor(1+Math.random()*6)
+    // dice.d3=Math.floor(1+Math.random()*6)
 
     document.getElementById("dice").innerHTML=`${dice.d1} ${dice.d2} ${dice.d3}`
 
     //checks if there is a 4-5-6 sequence
-        let roll=Object.values(dice)
+        let roll=Object.values(dice)//converts dice object into an array
+
     if(roll.includes(4) && roll.includes(5) && roll.includes(6)){
         console.log("yatta")
     }
@@ -55,7 +59,9 @@ function checkVal(){
 // }
 
 function setPointVal(){
+    //Object.values is a built in method that turns object values into an array
     let roll=Object.values(dice)
-    console.log(roll)
+    let result = roll.filter((value) => roll.indexOf(value) === roll.lastIndexOf(value));
+    return result.toString()
 }
 }
